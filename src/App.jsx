e09bc8b4fc562e2d85736381077acc22e9fa0ae7
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-06-30 17:46:08 +0300

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 
 export default function App() {
-    const [contacts, setContacts] = useState([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]);
-
+    const [contacts, setContacts] = useState(() => {
+        const storedContacts = localStorage.getItem('contacts');
+        return storedContacts ? JSON.parse(storedContacts) : [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+    });
     const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts]);
 
     const addContact = (newContact) => {
         setContacts((prevContacts) => [...prevContacts, newContact]);
@@ -35,4 +41,3 @@ export default function App() {
         </div>
     );
 }
-
